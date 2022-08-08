e32d462917645e5942414f09f8fdf2b45c6eb7a5
Culganosi <culgan_@hotmail.com>
2022-08-07 21:04:00 -0400

    remove cart items

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,20 +1,20 @@
-import React from "react";
+import { Fragment } from 'react';
 
-import HeaderCartButton from "./HeaderCartButton";
-import classes from '../Layout/Header.module.css'
-import mealsImg from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
 const Header = (props) => {
   return (
-    <>
+    <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
-        <div className={classes['main-image']}>
-          <img src={mealsImg} alt="Table of food" />
-        </div>
-    </>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
   );
 };
 
--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,43 +1,42 @@
-import { useReducer } from "react";
+import { useReducer } from 'react';
 
-import CartContext from "./cart-context";
+import CartContext from './cart-context';
 
 const defaultCartState = {
   items: [],
-  totalAmount: 0,
+  totalAmount: 0
 };
+
 const cartReducer = (state, action) => {
-  if (action.type === "ADD_CART_ITEM") {
+  if (action.type === 'ADD_CART_ITEM') {
     const updatedItems = state.items.concat(action.item);
-    const updatedTotalAmount =
-      state.totalAmount + action.item.price * action.item.amount;
-      return {
-        items: updatedItems,
-        totalAmount: updatedTotalAmount
-      }
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
   }
   return defaultCartState;
 };
 
 const CartProvider = (props) => {
-  const [cartState, dispatchCartAction] = useReducer(
-    cartReducer,
-    defaultCartState
-  );
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
   const addItemToCartHandler = (item) => {
-    dispatchCartAction({ type: "ADD_CART_ITEM", item: item });
+    dispatchCartAction({type: 'ADD_CART_ITEM', item: item});
   };
 
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({ type: "REMOVE_CART_ITEM", id: id });
+    dispatchCartAction({type: 'REMOVE_CART_ITEM', id: id});
   };
 
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
-    additem: addItemToCartHandler,
+    addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
+
   return (
     <CartContext.Provider value={cartContext}>
       {props.children}
